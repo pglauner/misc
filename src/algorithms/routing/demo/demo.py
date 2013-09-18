70792b8dd54a34f0b8a0bbf850b6df242d8d8924
pglauner <patrick.oliver.glauner@gmail.com>
2013-09-18 18:13:37 +0200

    Algorithms: new demo structure

--- src/algorithms/routing/demo/demo.py
+++ src/algorithms/routing/demo/demo.py
@@ -4,13 +4,13 @@ Created on Sep 15, 2013
 @author: pglauner
 '''
 
-from shortest_path import shortest_path_search
-from lowest_cost import lowest_cost_search
-from a_star.simple import a_star as a_star_simple
-from a_star.queue_based import a_star as a_star_queue
-from test_graphs.test_graphs import tricky_graph
-from test_graphs.phrase_graphs import simple_phrase
-from test_graphs.graph_utils import action_cost, is_goal, successors
+from algorithms.routing.shortest_path import shortest_path_search
+from algorithms.routing.lowest_cost import lowest_cost_search
+from algorithms.routing.a_star.simple import a_star as a_star_simple
+from algorithms.routing.a_star.queue_based import a_star as a_star_queue
+from algorithms.routing.test_graphs.test_graphs import tricky_graph
+from algorithms.routing.test_graphs.phrase_graphs import simple_phrase
+from algorithms.routing.test_graphs.graph_utils import action_cost, is_goal, successors
 
 
 if __name__ == '__main__':
