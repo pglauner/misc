f1974b89f8afed11ce3476d595ba0dd10d38936e
pglauner <patrick.oliver.glauner@gmail.com>
2013-10-06 13:59:31 +0200

    Solrdemo: moved to "solrdemo" repository

--- src/solrdemo/searcher.py
+++
@@ -1,6 +0,0 @@
-import solr
-# create a connection to the Solr server
-s = solr.SolrConnection('http://localhost:8983/solr')
-response = s.query('title:Solr')
-for hit in response.results:
-    print '%s: %s' % (hit[u'score'], hit[u'title'][0])
