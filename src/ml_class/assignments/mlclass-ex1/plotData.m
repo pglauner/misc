f0e964da4889df97f94535a5b92d18c7859425e7
pglauner <patrick.oliver.glauner@gmail.com>
2013-12-05 22:34:01 +0100

    ML: new plotData exercise

--- src/ml_class/assignments/mlclass-ex1/plotData.m
+++ src/ml_class/assignments/mlclass-ex1/plotData.m
@@ -13,12 +13,10 @@ function plotData(x, y)
 % Hint: You can use the 'rx' option with plot to have the markers
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
-
 figure; % open a new figure window
-
-
-
-
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
+ylabel('Profit in $10,000s'); % Set the y−axis label
+xlabel('Population of City in 10,000s'); % Set the x−axis label
 
 
 % ============================================================
