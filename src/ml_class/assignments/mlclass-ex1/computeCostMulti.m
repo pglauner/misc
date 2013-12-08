1342cf0f415fff55e38664dd5c19c97b7ead1706
pglauner <patrick.oliver.glauner@gmail.com>
2013-12-08 16:50:52 +0100

    ML: new compute cost multi exercise

--- src/ml_class/assignments/mlclass-ex1/computeCostMulti.m
+++ src/ml_class/assignments/mlclass-ex1/computeCostMulti.m
@@ -13,8 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+m = size(X, 1);
 
-
+J = 1/(2*m) * sum((X * theta - y) .^ 2)
 
 
 % =========================================================================
