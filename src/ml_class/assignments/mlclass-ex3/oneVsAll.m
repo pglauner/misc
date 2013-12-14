5f1fbb6eb22a0e46f48a6389b87d53e22ef042aa
pglauner <patrick.oliver.glauner@gmail.com>
2013-12-14 18:04:34 +0100

    ML: new one vs all solution

--- src/ml_class/assignments/mlclass-ex3/oneVsAll.m
+++ src/ml_class/assignments/mlclass-ex3/oneVsAll.m
@@ -49,7 +49,15 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+% Set Initial theta
+initial_theta = zeros(n + 1, 1);
 
+% Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+
+for c=1:num_labels
+    all_theta(c,:) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+end
 
 
 
