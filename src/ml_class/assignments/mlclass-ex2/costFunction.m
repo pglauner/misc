f99e1bf144961b1df26259cc4c1aa6c71d2af0a9
pglauner <patrick.oliver.glauner@gmail.com>
2014-01-05 16:27:09 +0100

    ML: better cost function and gradient solution

--- src/ml_class/assignments/mlclass-ex2/costFunction.m
+++ src/ml_class/assignments/mlclass-ex2/costFunction.m
@@ -22,7 +22,7 @@ grad = zeros(size(theta));
 
 hypothesis = sigmoid(X * theta);
 
-J = -1/m * sum(y .* log(hypothesis) + (1 - y) .* log(1 - hypothesis));
+J = 1/m * (-y' * log(hypothesis) - (1 - y)' * log(1 - hypothesis));
 
 grad = 1/m * X' * (hypothesis - y);
 
