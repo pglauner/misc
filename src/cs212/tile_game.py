8b67d5e90ff7d15007862ac7d97283a78ef7acff
pglauner <patrick.oliver.glauner@gmail.com>
2014-02-26 19:06:37 +0100

    CS212: simpler tile game implementation
    * Changes environment from 1D to 2D.

--- src/cs212/tile_game.py
+++ src/cs212/tile_game.py
@@ -4,70 +4,77 @@ Created on Nov 9, 2013
 @author: pglauner
 '''
 from algorithms.routing.shortest_path import shortest_path_search
+from itertools import chain
 
 
 tile_games = [#Solvable 
-              ('E', 6, 5, 1, 3, 2, 7, 4, 0),
-              (6, 5, 1, 3, 2, 7, 4, 0, 'E'),
+              (('E', 6, 5),
+               (1, 3, 2),
+               (7, 4, 0)),
+              ((6, 5, 1),
+               (3, 2, 7),
+               (4, 0, 'E')),
 
               #Unsolvable
-              (6, 5, 1, 3, 2, 7, 0, 4, 'E'),
+              ((6, 5, 1),
+               (3, 2, 7),
+               (0, 4, 'E')),
 
               #Solvable
-              ('E', 1, 2, 3, 0, 4, 5, 6, 8, 9, 10, 7, 12, 13, 14, 11),
+              (('E', 1, 2, 3),
+                (0, 4, 5, 6),
+                (8, 9, 10, 7),
+                (12, 13, 14, 11)),
 
               #Time-consuming
-              #(10, 2, 5, 0, 13, 'E', 4, 6, 9, 3, 12, 1, 14, 8, 11, 7),
+              #((10, 2, 5, 0),
+              # (13, 'E', 4, 6),
+              # (9, 3, 12, 1),
+              # (14, 8, 11, 7)),
               ]
 
 
-tile_lens = {1:1, 4:2, 9:3, 16:4}
+def get_y(pos, N):
+    return pos / N
 
 
-def get_y_x(pos, N):
-    y = pos / N
-    x = pos % N
-    return y, x
+def successors(state):
+    def generate_row_successors(state, transpose=False):
+        """
+        Generates row successors of a state.
+        """
+        original_state = state
+        if transpose:
+            state = tuple(zip(*state))
 
-
-def successors(N):
-    def move_row_or_col(state, res, idx1, idx2, action, offset=1):
-        squashed_values = [v for v in state[idx1:idx2:offset] if v != 'E']
-        for i in range(N):
+        N = len(state)
+        y = get_y(list(chain(*state)).index('E'), N)
+        row = list(state[y])
+        row.remove('E')
+        new_rows = [tuple(row[:i] + ['E'] + row[i:]) for i in xrange(N)]
+        successors = {}
+        for new_row in new_rows:
             new_state = list(state)
-            new_state[idx1:idx2:offset] = tuple(squashed_values[:i] + ['E'] + squashed_values[i:])
+            new_state[y] = new_row
             new_state = tuple(new_state)
-            if new_state != state:
-                res[new_state] = action
+            if transpose:
+                new_state = tuple(zip(*new_state))
+            if new_state <> original_state:
+                successors[new_state] = 'Col' if transpose else 'Row'
+        return successors
 
-    def successors_n(state):
-        res = {}
-        empty_y, empty_x = get_y_x(state.index('E'), N)
-
-        row_idx1 = empty_y * N
-        row_idx2 = row_idx1 + N
-        move_row_or_col(state, res, row_idx1, row_idx2, 'Row')
-
-        col_idx1 = empty_x
-        col_idx2 = N * (N - 1) + empty_x % N + 1
-        move_row_or_col(state, res, col_idx1, col_idx2, 'Col', N)
-
-        return res
-
-    return successors_n
+    return dict(generate_row_successors(state).items()
+                + generate_row_successors(state, True).items())
 
 
-def is_goal(N):
-    def is_goal_n(state):
-        return tuple(range(N*N-1) + ['E']) == state
-
-    return is_goal_n
+def is_goal(state):
+    N = len(state)
+    return tuple(chain(*state)) == tuple(range(N*N-1) + ['E'])
 
 
 if __name__ == '__main__':
     for start in tile_games:
-        N = tile_lens[len(start)]
         print start
-        shortest_path = shortest_path_search(start, successors(N), is_goal(N))
+        shortest_path = shortest_path_search(start, successors, is_goal)
         print shortest_path
         print len(shortest_path) / 2, 'moves'
