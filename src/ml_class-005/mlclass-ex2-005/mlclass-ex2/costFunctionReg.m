84ab6f647aabdc9a37561415eb52edf71e874e99
pglauner <patrick.oliver.glauner@gmail.com>
2014-03-24 17:23:51 +0100

    ML2: Ex2: new regularized cost function solution

--- src/ml_class-005/mlclass-ex2-005/mlclass-ex2/costFunctionReg.m
+++ src/ml_class-005/mlclass-ex2-005/mlclass-ex2/costFunctionReg.m
@@ -17,7 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+hypothesis = sigmoid(X * theta);
+theta2 = [0; theta(2:size(theta))];
 
+J = 1/m * (-y' * log(hypothesis) - (1 - y)' * log(1 - hypothesis));
+J += lambda / (2 * m) * sum(theta2.^2);
+
+grad = 1/m * X' * (hypothesis - y) + lambda / m * theta2;
 
 
 
