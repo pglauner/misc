2b9a81f7720ac20834c887a59cb46028c241df96
pglauner <patrick.oliver.glauner@gmail.com>
2014-03-31 22:23:18 +0200

    ML2: Ex3: new one-vs-all solution

--- src/ml_class-005/mlclass-ex3-005/mlclass-ex3/oneVsAll.m
+++ src/ml_class-005/mlclass-ex3-005/mlclass-ex3/oneVsAll.m
@@ -50,7 +50,15 @@ X = [ones(m, 1) X];
 %
 
 
+% Set Initial theta
+initial_theta = zeros(n + 1, 1);
 
+% Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+
+for c=1:num_labels
+    all_theta(c,:) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+end
 
 
 
