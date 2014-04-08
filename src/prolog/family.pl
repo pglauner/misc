3703a1465c47df6a5f4b382fd62ddd3185a56874
pglauner <patrick.oliver.glauner@gmail.com>
2014-04-08 20:10:49 +0200

    Prolog: richer family KB

--- src/prolog/family.pl
+++ src/prolog/family.pl
@@ -1,6 +1,33 @@
+different(X,X) :- !,fail.
+different(_,_).
+
 parent(pam, bob).
 parent(tom, bob).
 parent(tom, liz).
 parent(bob, ann).
 parent(bob, pat).
 parent(pat, jim).
+
+female(pam).
+female(liz).
+female(pat).
+female(ann).
+male(tom).
+male(bob).
+male(jim).
+
+offspring(Y, X) :- parent(X, Y).
+
+mother(X, Y) :-
+  parent(X, Y),
+  female(X).
+
+grandparent(X, Z) :-
+  parent(X, Y),
+  parent(Y, Z).
+
+sister(X, Y) :-
+  parent(Z, X),
+  parent(Z, Y),
+  female(X),
+  different(X, Y).
