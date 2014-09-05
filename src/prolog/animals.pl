e8e912fb823db9da46dd4f278e8762d0df278171
pglauner <patrick.oliver.glauner@gmail.com>
2014-09-05 12:19:14 +0200

    Prolog: new sample files

---
+++ src/prolog/animals.pl
@@ -0,0 +1,10 @@
+big(bear).
+big(elephant).
+small(cat).
+
+brown(bear).
+black(cat).
+gray(elephant).
+
+dark(Z) :- black(Z).
+dark(Z) :- brown(Z).
---
+++ src/prolog/geometry.pl
@@ -0,0 +1,2 @@
+vertical(seg(point(X,Y), point(X,Y1))).
+horizontal(seg(point(X,Y), point(X1,Y))).
